33c67123862974f50e94dc68f8ec5b97f8f55d2a
Dan R. K. Ports <drkp@cs.washington.edu>
2016-01-07 16:38:07 -0800

    Manage /etc/default file and set autostart

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,7 @@ class openvpn(
   $openvpn_dir    = $openvpn::params::openvpn_dir,
   $package_name   = $openvpn::params::package_name,
   $manage_service = $openvpn::params::manage_service,
+  $autostart_all  = $openvpn::params::autostart_all
 ) inherits openvpn::params {
 
   package { $package_name:
@@ -25,6 +26,21 @@ class openvpn(
       ensure  => running,
       enable  => true,
       require => File[$openvpn_dir],
+    }
+
+    if $::osfamily == 'Debian' {
+      concat { '/etc/default/openvpn':
+        owner => root,
+        group => 0,
+        mode  => '0644',
+        warn  => true,
+      }
+      
+      concat::fragment { 'openvpn.default.header':
+        content => template('openvpn/etc-default-openvpn.erb'),
+        target  => '/etc/default/openvpn',
+        order   => '01',
+      }
     }
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -9,6 +9,7 @@ class openvpn::params {
       $openvpn_dir    = '/usr/local/etc/openvpn'
       $package_name   = 'security/openvpn'
       $manage_service = true
+      $autostart_all  = true
       $openvpn_user   = undef
       $openvpn_group  = undef
       $openssl        = '/usr/bin/openssl'
@@ -17,6 +18,7 @@ class openvpn::params {
       $openvpn_dir    = '/etc/openvpn'
       $package_name   = 'openvpn'
       $manage_service = false
+      $autostart_all  = false
       $openvpn_user   = '_openvpn'
       $openvpn_group  = '_openvpn'
       if ( versioncmp($::kernelversion, '5.7') < 0 ) {
@@ -29,6 +31,7 @@ class openvpn::params {
       $openvpn_dir    = '/etc/openvpn'
       $package_name   = 'openvpn'
       $manage_service = true
+      $autostart_all  = true
       $openvpn_user   = 'nobody'
       $openvpn_group  = 'nobody'
       $openssl        = '/usr/bin/openssl'
